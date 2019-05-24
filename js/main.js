2dd49c2dc73f7dcd39bc00365fa8e27e2e1c0396
olejo-hypero <programpsc@gmail.com>
2019-05-24 16:28:40 +0300

    c
    c

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+          autoplay: true,
+          autoplaySpeed: 2000,
+          arrows: true,
+          slidesToShow: 2,
+    });
+});
